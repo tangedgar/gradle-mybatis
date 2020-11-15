1e4d13cdc498970c20d6d42cbfbc6a906b9e4f61
唐煇 <tang_edgar@163.com>
2020-11-15 12:00:38 +0800

    Log4j日志

--- mybatis-03/src/test/java/com/tanghui03/dao/UserMapperTest.java
+++ mybatis-03/src/test/java/com/tanghui03/dao/UserMapperTest.java
@@ -3,11 +3,15 @@ package com.tanghui03.dao;
 import com.tanghui03.model.User;
 import com.tanghui03.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.util.List;
 
 public class UserMapperTest {
+
+    static Logger logger = Logger.getLogger(UserMapperTest.class);
+
     @Test
     public void getUserListTest(){
         SqlSession sqlSession = MybatisUtils.getSqlSession();
@@ -19,6 +23,13 @@ public class UserMapperTest {
         }
 
         sqlSession.close();
+    }
+
+    @Test
+    public void log4jTest(){
+        logger.info("info:进入了log4jTest");
+        logger.debug("debug:进入了log4jTest");
+        logger.error("error:进入了log4jTest");
     }
 
     public static void main(String[] args) {
